3a7f5f44c641848b9e31e6773c54f70db0311587
bruno <brunomalaquias012345@gmail.com>
2025-03-21 19:28:28 -0300

    fix: arrumando a listagem de livros por ano em ordem crescente

--- GestaoBiblioteca/src/main/java/org/example/biblioteca/Biblioteca.java
+++ GestaoBiblioteca/src/main/java/org/example/biblioteca/Biblioteca.java
@@ -7,6 +7,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Biblioteca {
 
     private List<Livro> livros = new ArrayList<>();
