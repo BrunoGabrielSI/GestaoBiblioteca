4422ce2901620b906a2e5c41fa8c0cdaae762890
BrunoGabrielSI <99301997+BrunoGabrielSI@users.noreply.github.com>
2025-03-21 17:17:20 -0300

    feat: adicionando lista de livros na classe Biblioteca

--- GestaoBiblioteca/src/main/java/org/example/biblioteca/Biblioteca.java
+++ GestaoBiblioteca/src/main/java/org/example/biblioteca/Biblioteca.java
@@ -1,4 +1,11 @@
 package org.example.biblioteca;
 
+import org.example.model.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+
+    private List<Livro> livros = new ArrayList<>();
 }
